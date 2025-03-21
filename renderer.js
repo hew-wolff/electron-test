4c84881b64c201bf29fe25ab3d05fc31b33872d7
Hew Wolff <hew@hewwolff.org>
2025-03-21 10:51:22 -0700

    clean

--- renderer.js
+++ renderer.js
@@ -1,37 +1,62 @@
-const initialize = () => {
-  console.log('init');
+const initialize = async () => {
+  console.log('init')
   const recordStart = document.querySelector('.recordStart')
   const recordEnd = document.querySelector('.recordEnd')
   const videoElement = document.querySelector('.video')
 
+    /*
   recordStart.disabled = true
   recordEnd.disabled = true
+*/
 
-  const newMediaRecorder = (mimeType, mimeTypeFileExtension) => {
+  const newMediaRecorder = async (mimeType, mimeTypeFileExtension) => {
+    let result        
     if (!(navigator.mediaDevices?.getUserMedia)) {
       console.log("getUserMedia not supported on your browser!")
       return undefined
     }
+    console.log('get stream')
+    try {
+      let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+      console.log('got stream')
+      result = new MediaRecorder(stream, { mimeType: mimeType })
+      console.log('MediaRecorder: ' + JSON.stringify(result))
+      console.log('mimeType: ' + JSON.stringify(result.mimeType))
+      result.ondataavailable = async (e) => {
+        console.log('recorder got data')
+        await displayVideo(e.data, mimeType, mimeTypeFileExtension)
+      }
+    } catch (err) {
+      console.error(`The following getUserMedia error occurred: ${err}`)
+      return undefined
+    }
+          /*
     navigator.mediaDevices.getUserMedia({ video: true, audio: true }).
       then((stream) => {
         console.log('got stream')
-        mediaRecorder = new MediaRecorder(stream, { mimeType: mimeType });
-        console.log('MediaRecorder: ' + JSON.stringify(mediaRecorder));
-        console.log('mimeType: ' + JSON.stringify(mediaRecorder.mimeType));
-        mediaRecorder.ondataavailable = async (e) => {
+        result = new MediaRecorder(stream, { mimeType: mimeType })
+        console.log('MediaRecorder: ' + JSON.stringify(result))
+        console.log('mimeType: ' + JSON.stringify(result.mimeType))
+        result.ondataavailable = async (e) => {
           console.log('recorder got data')
           await displayVideo(e.data, mimeType, mimeTypeFileExtension)
         }
-        return mediaRecorder
     }).
     catch((err) => {
       console.error(`The following getUserMedia error occurred: ${err}`)
       return undefined
     })
+*/
+    console.log('returning result: ' + JSON.stringify(result))
+    return result
   }
 
   // Seems to work better than MP4.
-  let mediaRecorder = newMediaRecorder('video/webm', 'webm');
+    const mediaRecorder = await newMediaRecorder('video/webm', 'webm')
+    /*
+  console.log('outer MediaRecorder: ' + JSON.stringify(mediaRecorder))
+  console.log('outer mimeType: ' + JSON.stringify(mediaRecorder.mimeType))
+*/
 
   recordStart.onclick = () => {
     recordStart.disabled = true
