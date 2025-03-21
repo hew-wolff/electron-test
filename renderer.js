bacd8854125451f5270f68f1253c94c3474a678f
Hew Wolff <hew@hewwolff.org>
2025-03-21 09:22:30 -0700

    clean

--- renderer.js
+++ renderer.js
@@ -2,39 +2,41 @@ const initialize = () => {
   console.log('init');
   const recordStart = document.querySelector('.recordStart')
   const recordEnd = document.querySelector('.recordEnd')
+  const videoElement = document.querySelector('.video')
 
   recordStart.disabled = true
   recordEnd.disabled = true
 
-  if (!(navigator.mediaDevices?.getUserMedia)) {
-    console.log("getUserMedia not supported on your browser!")
-    return
-  }
-
-  // Seems to work better than MP4.
-  const mimeType = 'video/webm'
-  const mimeTypeFileExtension = 'webm'
-  let mediaRecorder;
-  let chunks = [];
-
-  // TODO fix bug: first attempt at recording gets 0 bytes.
-  navigator.mediaDevices.getUserMedia({ video: true, audio: true }).
-    then((stream) => {
+  const newMediaRecorder = (mimeType) => {
+    if (!(navigator.mediaDevices?.getUserMedia)) {
+      console.log("getUserMedia not supported on your browser!")
+      return undefined
+    }
+    navigator.mediaDevices.getUserMedia({ video: true, audio: true }).
+      then((stream) => {
         console.log('got stream')
         mediaRecorder = new MediaRecorder(stream, { mimeType: mimeType });
         console.log('MediaRecorder: ' + JSON.stringify(mediaRecorder));
         console.log('mimeType: ' + JSON.stringify(mediaRecorder.mimeType));
         mediaRecorder.ondataavailable = async (e) => {
           console.log('recorder got data')
-            chunks.push(e.data)
-            await displayVideo(e.data)
+          await displayVideo(e.data)
         }
+        return mediaRecorder
     }).
     catch((err) => {
       console.error(`The following getUserMedia error occurred: ${err}`)
       recordStart.disabled = false
       recordEnd.disabled = true
+
+      return undefined
     })
+  }
+
+  // Seems to work better than MP4.
+  const mimeType = 'video/webm'
+  const mimeTypeFileExtension = 'webm'
+  let mediaRecorder = newMediaRecorder(mimeType);
 
   recordStart.onclick = () => {
     recordStart.disabled = true
@@ -44,39 +46,21 @@ const initialize = () => {
     console.log('recorder started: ' + mediaRecorder.state)
   }
 
-  const displayVideo = async (chunk) => {
-    const blob = new Blob([chunk], { type: mimeType })
-    //const blob = mediaRecorder.requestData()
-    //chunks = undefined
-    console.log(`got blob with ${blob.size} bytes`)
-    const arrayBuffer = await blob.arrayBuffer()
-    window.main.saveFile(arrayBuffer, mimeTypeFileExtension)
-
-    const videoElement = document.querySelector('.video')
-    const videoUrl = URL.createObjectURL(blob)
-    videoElement.src = videoUrl
-    videoElement.load()
-  }
-
   recordEnd.onclick = async () => {
     recordStart.disabled = false
     recordEnd.disabled = true
     mediaRecorder.stop()
     console.log('recorder stopped: ' + mediaRecorder.state)
+  }
 
-      /*
-    const blob = new Blob(chunks, { type: mimeType })
-    //const blob = mediaRecorder.requestData()
-    //chunks = undefined
+  const displayVideo = async (chunk) => {
+    const blob = new Blob([chunk], { type: mimeType })
     console.log(`got blob with ${blob.size} bytes`)
     const arrayBuffer = await blob.arrayBuffer()
     window.main.saveFile(arrayBuffer, mimeTypeFileExtension)
 
-    const videoElement = document.querySelector('.video')
-    const videoUrl = URL.createObjectURL(blob)
-    videoElement.src = videoUrl
+    videoElement.src = URL.createObjectURL(blob)
     videoElement.load()
-*/
   }
 
   recordStart.disabled = false
